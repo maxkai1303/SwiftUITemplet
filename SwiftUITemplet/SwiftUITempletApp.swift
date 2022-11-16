0192573aea1363552ea793d9611f65c9e4f74812
Max <k1071303@gmail.com>
2022-11-16 21:10:55 +0800

    Open project

---
+++ SwiftUITemplet/SwiftUITempletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITempletApp.swift
+//  SwiftUITemplet
+//
+//  Created by Max Kai on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITempletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
